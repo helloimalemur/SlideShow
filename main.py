580aeada75a966672e5c193c4175fb2ca861f2a8
foxx <jkoontsiii@gmail.com>
2022-07-04 17:09:38 -0400

    y

--- main.py
+++ main.py
@@ -1,26 +1,40 @@
+from kivy.clock import Clock
 from kivy.app import App
 from kivy.uix.carousel import Carousel
 from kivy.uix.label import Label
 from kivy.uix.image import AsyncImage
 from kivy.uix.boxlayout import BoxLayout
 import glob
+import random
 import time
 import asyncio
 import kivy
 
+
+def getimages(self):
+    self.images = glob.glob("/home/foxx/Pictures/*.jpg")
+    return self.images
+
+def update(self):
+    print(time.time())
+
 class SlideShow(App):
     def build(self):
+        Clock.schedule_interval(update, 2)
         #create child widget
         carousel = Carousel(direction='right')
 
         #load images
-        images = glob.glob("/home/foxx/Pictures/*.jpg")
-
-        #for i in images:
+        images = getimages(self)
+        for i in images:
+            print(i)
+            image = AsyncImage(source=i)
+            carousel.add_widget(image)
 
         ## create root layout and add widgets
         root = BoxLayout()
         root.add_widget(carousel)
+
         return root
 
 

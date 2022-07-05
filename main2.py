8777b61c33f1d4e6c6ff7b36411fdd3ac6c10a71
foxx <jkoontsiii@gmail.com>
2022-07-05 12:24:56 -0400

    y

--- main2.py
+++ main2.py
@@ -2,45 +2,71 @@ from kivy.clock import Clock
 from kivy.app import App
 from kivy.uix.carousel import Carousel
 from kivy.uix.label import Label
-from kivy.uix.image import AsyncImage, Image
-from kivy.uix.gridlayout import GridLayout
-
+from kivy.uix.image import AsyncImage
+from kivy.uix.boxlayout import BoxLayout
 import glob
+import random
 import time
 import asyncio
+import kivy
 
-class SlideShowApp(App):
+
+def getimages(self):
+    self.images = glob.glob("/home/foxx/Pictures/*.jpg")
+    #for i in self.images:
+    #    print(i)
+    #    image = AsyncImage(source=i)
+    #    self.carousel.add_widget(image)
+    return self.images
+
+#if empty set imagelist as first 100 images, if not empty, diff against main list - take next 100 images not used already and set imagelist
+#diff against usedimages
+def genlist(self):
+    if self.used:
+        self.used = list(set(self.used) + set(self.using))
+        self.using = ()
+        self.using = list(set(self.images) | set(self.used))
+        self.using = self.using[0:99]
+    else:
+        self.used = ()
+        self.using = list(set(self.images[0:99]))
+    return self.using
+
+def loadimages(self, im):
+    for i in im:
+        print(i)
+    image = AsyncImage(source=str(i))
+    self.carousel.add_widget(image)
+
+class SlideShow(App):
     def build(self):
-        slideshow = SlideShowApp()
-        #x = self.window()
-        #layout.add_widget(x)
-        return slideshow
+        self.used = ()
+        Clock.schedule_interval(self.update, 3)
+        #create child widget
+        self.carousel = Carousel(direction='right')
+        #load images
+        self.images = getimages(self)
 
-    def window(self):
 
-        self.c = Carousel(direction='right')
-        #Clock.schedule_interval(self.update, 2)
+        ## create root layout and add widgets
+        root = BoxLayout()
+        root.add_widget(self.carousel)
 
-        for i in SlideShowApp.getimages():
-            self.curimage = AsyncImage(source=i)
-            self.c.add_widget(self.curimage)
-            print(self.curimage)
-            # time.sleep(3)
-            # c.next_slide()
-
-        return self.c
-
-    def getimages():
-        images = glob.glob("/home/foxx/Pictures/*.jpg")
-        return images
+        return root
 
     def update(self, *args):
         print(time.time())
+        x = genlist(self)
+        loadimages(self, x)
+        if self.carousel.next_slide:
+            self.carousel.load_next()
+        else:
+            self.carousel.clear_widgets()
+            getimages(self)
 
 
-class Layout(GridLayout):
-    def __init__(self):
-        self.c = None
 
 if __name__ == '__main__':
-    SlideShowApp().run()
+    SlideShow().run()
+
+

3be06e9855348c100ab7332a320ec905288e5e14
foxx <jkoontsiii@gmail.com>
2022-07-05 14:13:45 -0400

    reduced memory usage

--- main2.py
+++ main2.py
@@ -12,62 +12,65 @@ import kivy
 
 
 def getimages(self):
-    self.images = glob.glob("/home/foxx/Pictures/*.jpg")
-    #for i in self.images:
-    #    print(i)
-    #    image = AsyncImage(source=i)
-    #    self.carousel.add_widget(image)
-    return self.images
+    print("create image glob")
+    self.imageglob = glob.glob("/home/foxx/Pictures/*.jpg")
+    return self.imageglob
 
-#if empty set imagelist as first 100 images, if not empty, diff against main list - take next 100 images not used already and set imagelist
-#diff against usedimages
+
+# if empty set imagelist as first 100 images, if not empty, diff against main list - take next 100 images not used already and set imagelist
+# diff against usedimages
 def genlist(self):
-    if self.used:
-        self.used = list(set(self.used) + set(self.using))
-        self.using = ()
-        self.using = list(set(self.images) | set(self.used))
-        self.using = self.using[0:99]
-    else:
-        self.used = ()
-        self.using = list(set(self.images[0:99]))
+    print("generating and reducing list")
+    self.pused = self.using
+    self.tused = list(set(self.pused) | set(self.tused))
+    us = list(set(self.ig) - set(self.tused))
+    self.using = us[0:20] #set ims actually loaded
     return self.using
 
+
 def loadimages(self, im):
+    print("loading images into carousel")
+    #im = genlist(self)
     for i in im:
         print(i)
-    image = AsyncImage(source=str(i))
-    self.carousel.add_widget(image)
+        image = AsyncImage(source=str(i))
+        self.carousel.add_widget(image)
 
 
 class SlideShow(App):
     def build(self):
+        print("building app")
+        self.imageglob = getimages(self)
+        self.ig = self.imageglob
         self.used = ()
-        Clock.schedule_interval(self.update, 3)
-        #create child widget
+        self.using = ()
+        self.tused = ()
+        self.pused = ()
+        Clock.schedule_interval(self.update, 4)
+        # create child widget
         self.carousel = Carousel(direction='right')
-        #load images
-        self.images = getimages(self)
-
-
+        # load images
+        # self.images = getimages(self)
         ## create root layout and add widgets
         root = BoxLayout()
         root.add_widget(self.carousel)
-
         return root
 
     def update(self, *args):
+        print("updating..")
         print(time.time())
-        x = genlist(self)
-        loadimages(self, x)
+        #x = genlist(self)
+        #loadimages(self, x)
         if self.carousel.next_slide:
+            print("swiping")
             self.carousel.load_next()
         else:
+            self.tused = ()
+            x = genlist(self)
             self.carousel.clear_widgets()
-            getimages(self)
-
+            loadimages(self, x)
+            #getimages(self)
 
 
 if __name__ == '__main__':
     SlideShow().run()
-
-
